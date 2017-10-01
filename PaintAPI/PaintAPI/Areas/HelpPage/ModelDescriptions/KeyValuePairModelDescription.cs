47685bb234a5808877a92654c3adf8f015c3a2ae
Unknown <rostyktymk1896@gmail.com>
2017-10-01 19:28:56 +0300

    Delete
    all redundand

---
+++ PaintAPI/PaintAPI/Areas/HelpPage/ModelDescriptions/KeyValuePairModelDescription.cs
@@ -0,0 +1,9 @@
+namespace PaintAPI.Areas.HelpPage.ModelDescriptions
+{
+    public class KeyValuePairModelDescription : ModelDescription
+    {
+        public ModelDescription KeyModelDescription { get; set; }
+
+        public ModelDescription ValueModelDescription { get; set; }
+    }
+}
---
+++ PaintAPI/PaintAPI/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="PaintAPI.WebApiApplication" Language="C#" %>
